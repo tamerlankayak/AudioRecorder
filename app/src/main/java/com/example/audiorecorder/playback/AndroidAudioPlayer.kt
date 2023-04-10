b10c0a890c831160117363c368e2faebd00e1f50
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-04-10 23:35:08 +0400

    Update AndroidAudioPlayer.kt

--- app/src/main/java/com/example/audiorecorder/playback/AndroidAudioPlayer.kt
+++ app/src/main/java/com/example/audiorecorder/playback/AndroidAudioPlayer.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.media.MediaPlayer
 import androidx.core.net.toUri
 import java.io.File
-
+//creating actions of player
 class AndroidAudioPlayer(private val context: Context) : AudioPLayer {
     private var player: MediaPlayer? = null
 
